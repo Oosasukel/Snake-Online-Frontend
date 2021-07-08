623997611211c15bddf5c4fae944d660ba876e3a
Rodrigo Gonçalves <rodrigo_gonn@hotmail.com>
2021-07-07 23:58:55 -0300

    Initial Config

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+
+import Head from '../components/Head';
+
+import '../styles/globals.css';
+import { theme } from '../theme';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Head title="Snake Online" />
+
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import Home from '../pagesComponents/Home';
+
+const HomePage = () => {
+  return <Home />;
+};
+
+export default HomePage;
